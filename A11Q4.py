358bab8e1d1be3e4fc22227a3737e3a7cfc2aa7f
Mrunmay23 <mrunmaywadekar02@gmail.com>
2026-01-22 01:28:15 +0530

    A11Q4.py

--- A11Q4.py
+++ A11Q4.py
@@ -1,12 +1,18 @@
-# input 123
-print("Enter Number :")
-n = int(input())
+def ReverseNumber(No):
+    Rev = 0
+    
+    while No > 0:
+        reminder = No % 10
+        Rev = Rev * 10 + reminder
+        No = No // 10
 
-rev = 0
+    return Rev
 
-while n > 0:
-    rev = rev * 10 + (n % 10)
-    n //= 10
+def main():
+    Value = int(input("Enter Number: "))
+    Ret = ReverseNumber(Value)
 
-print(rev)
-#output 321
+    print("Reversed Number is: ",Ret)
+
+if __name__ == "__main__" :
+    main()
