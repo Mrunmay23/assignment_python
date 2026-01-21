eff3f1226a4d7b8d2c13c5565f544bbb6c796101
Mrunmay23 <mrunmaywadekar02@gmail.com>
2026-01-22 01:25:31 +0530

    A10Q5.py

--- A10Q5.py
+++ A10Q5.py
@@ -1,9 +1,13 @@
+def Odd(No):
+    
+    for i in range(1,No+1,2):    
+        print(i,end=" ")
+
 def main():
+    Value = int(input("Enter Number : "))
 
-    no = int(input("Enter Number: "))
+    Odd(Value)
+    
 
-    for i in range(1, no+1, 2):
-        print(i, end=" ")
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
