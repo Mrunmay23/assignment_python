ef431fa1c7197f9afe4d2251a17f7c0195af5909
Mrunmay23 <mrunmaywadekar02@gmail.com>
2026-01-22 01:26:35 +0530

    A11Q1.py

--- A11Q1.py
+++ A11Q1.py
@@ -1,8 +1,18 @@
-def Checkprime(n):
-    for i in range(2, n):
-        if n % i == 0:
-            print("Not Prime")
-            return
-    print("Prime:",n)
+def ChkPrime(No):
+    if(No <= 1):
+        return False
+    for i in range(2,No):
+        if(No % i == 0):
+            return False
 
-Checkprime(11)
+    else:
+        return True
+     
+def main():
+    Value = int(input("Enter Number: "))
+    Ret = ChkPrime(Value)
+
+    if(Ret == True):
+        print("Prime Number")
+    else:
+        print("Not a prime Number")
