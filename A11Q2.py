19d534ad0c0a0f59904943844c58f60e54ade7f0
Mrunmay23 <mrunmaywadekar02@gmail.com>
2026-01-22 01:27:17 +0530

    A11Q2.py

--- A11Q2.py
+++ A11Q2.py
@@ -1,6 +1,18 @@
-num = int(input())   # Input: 7521
-count = 0
-while num > 0:
-    count += 1
-    num //= 10
-print(count)         # Output: 4
+def CountDigits(No):
+    count = 0
+    if(No == 0):
+        return 1
+    while No > 0:
+        No = No // 10
+        count = count + 1
+
+    return count
+
+def main():
+    Value = int(input("Enter Number: "))
+    Ret = CountDigits(Value)
+
+    print("Count of Digits is: ",Ret)
+
+if __name__ == "__main__" :
+    main()
