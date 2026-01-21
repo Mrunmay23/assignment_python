b125e477bbc982dc6fd4937c9dea5251745e8ecf
Mrunmay23 <mrunmaywadekar02@gmail.com>
2026-01-22 01:25:02 +0530

    A10Q4.py

--- A10Q4.py
+++ A10Q4.py
@@ -1,4 +1,13 @@
-n = int(input())
+def Even(No):
+    
+    for i in range(2,No+1,2):    
+        print(i,end=" ")
 
-for i in range(2, n + 1, 2):
-    print(i, end="")
+def main():
+    Value = int(input("Enter Number : "))
+
+    Even(Value)
+    
+
+if __name__ == "__main__":
+    main()
