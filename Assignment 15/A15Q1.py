d0cd2598e79cc32d6c4c9311d94998f226a05eab
Mrunmay23 <mrunmaywadekar02@gmail.com>
2026-01-25 14:53:35 +0530

    Assignment 15

--- Assignment 15/A15Q1.py
+++
@@ -1,9 +0,0 @@
-Square = lambda No : No ** 2
-
-def main():
-    Value = int(input("Enter Number: "))
-    Ret = Square(Value)
-    print("Square is: ",Ret)
-
-if __name__ == "__main__":
-    main()
--- Assignment 15/A15Q2.py
+++
@@ -1,9 +0,0 @@
-Cube = lambda No : No ** 3
-
-def main():
-    Value = int(input("Enter Number: "))
-    Ret = Cube(Value)
-    print("Cube is: ",Ret)
-
-if __name__ == "__main__":
-    main()
